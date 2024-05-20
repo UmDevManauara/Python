79c1888f8865430ffeab49336763752c63da6592
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-05-20 17:02:37 -0400

    att

--- Aula 1/novo.py
+++ Aula 1/novo.py
@@ -5,7 +5,7 @@ nome = input("Qual seu nome?")
 
 print(f"ola, {nome}, seja bem-vindo")
 
-idade = int(input("Qal a sua idade?"))
+idade = int(input("Qual a sua idade?"))
 if idade <13:
     print ("Criança")
 elif idade < 18:
@@ -14,3 +14,10 @@ elif idade < 60:
     print("adulto")
 else: 
     print("idoso")
+
+print (int(4/2))
+
+print (round(2/3,3))
+x = 1/7
+
+print(f"{x:.50f}")
